66c5fa8536fd7af925663f681cdbd846a8c14a16
Ryan Castner <castner.rr@gmail.com>
2019-10-14 17:07:35 -0400

    feat: AuthProvider implementation and testing

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,37 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { AuthContext, AuthProvider, AuthProviderProps } from '../.';
 
-const App = () => {
-	return (
-		<div>
-			<Thing />
-		</div>
-	);
-};
+function DisplayAuthenticated() {
+  const { authenticated } = React.useContext(AuthContext);
+  return <p data-testid="authenticated">{authenticated ? 'true' : 'false'}</p>;
+}
+
+function Button() {
+  const { authenticated, setAuthenticated } = React.useContext(AuthContext);
+  return (
+    <button
+      data-testid="button"
+      type="button"
+      onClick={() => setAuthenticated(!authenticated)}
+    >
+      Toggle Authenticated
+    </button>
+  );
+}
+
+function App({ onLogin, onLogout, defaultAuthenticated }: AuthProviderProps) {
+  return (
+    <AuthProvider
+      onLogin={onLogin}
+      onLogout={onLogout}
+      defaultAuthenticated={defaultAuthenticated}
+    >
+      <DisplayAuthenticated />
+      <Button />
+    </AuthProvider>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
